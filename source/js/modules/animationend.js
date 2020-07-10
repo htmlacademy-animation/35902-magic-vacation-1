984c6c624bdd761712e7690fca88caddd8d654d8
Anna <apelsann@yandex.ru>
2020-07-10 05:55:06 +0300

    feat: add letters animation on the main screen

--- source/js/modules/animationend.js
+++ source/js/modules/animationend.js
@@ -2,15 +2,23 @@ export default () => {
   const bgFill = document.querySelector(`.bg-fill`);
   const lastRule = document.querySelector(`.rules__item:last-child p`);
   const rulesLink = document.querySelector(`.rules__link`);
-  const activeClass = `active`;
+  // const introTitleElement = document.querySelector(`.intro__title span:last-child`);
+  const introMessage = document.querySelector(`.intro__message`);
+  const animatedClass = `animated`;
 
   // the addEventListener below is for the cases of several screen with filled animation (currently, it's applicable for prizes screen only)
   bgFill.addEventListener(`animationend`, () => {
-    bgFill.classList.remove(activeClass);
+    bgFill.classList.remove(animatedClass);
   });
 
   lastRule.addEventListener(`animationend`, () => {
-    rulesLink.classList.add(activeClass);
+    rulesLink.classList.add(animatedClass);
   });
+
+  // introTitleElement.addEventListener(`transitionend`, () => {
+  //   introMessage.style.color = `red`;
+  //   introMessage.style.animationName = `introFadeIn`;
+  //   introMessage.style.animationDuration = `1s`;
+  // });
 
 };
